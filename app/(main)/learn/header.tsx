1130e09e6db6d3ced3b96d0d1a19d01966e41368
Edieson Dela Torre <popcakedflux@gmail.com>
2024-11-10 20:06:34 +0800

    3:fixed ui

--- app/(main)/learn/header.tsx
+++ app/(main)/learn/header.tsx
@@ -9,10 +9,13 @@ type HeaderProps = {
 
 export const Header = ({ title }: HeaderProps) => {
   return (
+    
+     
+    
     <div className="sticky top-0 mb-5 flex items-center justify-between border-b-2 bg-white pb-3 text-neutral-400 lg:z-50 lg:mt-[-28px] lg:pt-[28px]">
-      <Link href="/courses">
+       <Link href="/courses">
         <Button size="sm" variant="ghost">
-          <ArrowLeft className="h-5 w-5 stroke-2 text-neutral-400" />
+          <ArrowLeft className="h-8 w-8 stroke-4 text-neutral-500  " />
         </Button>
       </Link>
 
