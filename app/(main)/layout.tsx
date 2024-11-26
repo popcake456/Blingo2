cf826677f9e00c0d65bdf04c59629ef378c9757a
Edieson Dela Torre <popcakedflux@gmail.com>
2024-11-26 21:39:43 +0800

    7:fixed dictionary and remove pass

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,7 +8,7 @@ const MainLayout = ({ children }: PropsWithChildren) => {
     <>
       <MobileHeader />
       <Sidebar className="hidden lg:flex" />
-      <main className="h-full pt-[50px] lg:pl-[256px] lg:pt-0">
+      <main className="h-full pt-[50px] lg:pl-[256px] lg:pt-0 ">
         <div className="mx-auto h-full max-w-[1056px] pt-6">{children}</div>
       </main>
     </>
